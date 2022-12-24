5219a24cc9b661aed9e3a47bad1ddf09383f14a5
Nicolas <elendil_le_grand@hotmail.com>
2022-12-24 01:23:35 +0100

    Test resize

--- src/front/javascript/classes/ElementResizer.js
+++ src/front/javascript/classes/ElementResizer.js
@@ -7,7 +7,6 @@ export class ElementResizer {
 	static #offsetPosition
 	static #callBack
 	static #isPointerDown = false
-	static #keysPress = {}
 
 	static init (pElement, pOffset, pCallBack) {
 		this.#element = pElement
@@ -16,22 +15,24 @@ export class ElementResizer {
 		this.#resetMousePosition()
 		this.#createHandler()
 		// this.#selector.addEventListener('pointerdown', this.#pointerDown)
-		// document.body.addEventListener('pointermove', this.#pointerMove)
-		// this.#element.addEventListener('keydown', this.#keyDown)
-		// this.#element.addEventListener('keyup', this.#keyUp)
+		document.body.addEventListener('pointermove', this.#pointerMove)
 	}
 
 	static #resetMousePosition () {
 		this.#mouse = {
 			x: 0,
-			y: 0
+			y: 0,
+			translateX: 0,
+			translateY: 0,
+			originalX: 0,
+			originalY: 0
 		}
 	}
 
 	static #createHandler () {
 		const boundingBox = this.#element.getBoundingClientRect()
-		const boxesSize = 10
-		const offset = 5
+		const boxesSize = 20
+		const offset = 10
 		const boxes = [
 			{ top: 0, left: 0, class: 'leftTop', offset: { top: -boxesSize, left: -boxesSize } },
 			{ top: boundingBox.height / 2, left: 0, class: 'leftCenter', offset: { top: -offset, left: -boxesSize - offset } },
@@ -44,53 +45,70 @@ export class ElementResizer {
 		]
 		const bd = Dom.newDom(this.#element)
 		boxes.forEach((pBox) => {
-			bd.elt('div', `resizeHandler ${pBox.class}`).att('style', `position: absolute;top: ${pBox.top + pBox.offset.top}px;left: ${pBox.left + pBox.offset.left}px;width: ${boxesSize}px;height: ${boxesSize}px;border-radius: ${boxesSize}px;`).up()
+			if (!document.querySelector(`.${pBox.class}`)) {
+				bd.elt('div', `resizeHandler ${pBox.class}`).att('style', `position: absolute;top: ${pBox.top + pBox.offset.top}px;left: ${pBox.left + pBox.offset.left}px;width: ${boxesSize}px;height: ${boxesSize}px;border-radius: ${boxesSize}px;`).listen('pointerdown', this.#pointerDown).up()
+			}
 		})
 	}
 
-	//
-	// static async #pointerDown (pEvent) {
-	//	ElementMover.#selector.addEventListener('pointerup', ElementMover.#pointerUp)
-	//	ElementMover.#isPointerDown = true
-	// }
-	//
-	// static #pointerMove (pEvent) {
-	//	pEvent.returnValue = false
-	//	const translate = ElementMover.#element.style.translate.split(' ')
-	//	ElementMover.#offsetPosition = {
-	//		x: ElementMover.#offsetPosition.x + (ElementMover.#mouse.x - parseInt(translate[0])),
-	//		y: ElementMover.#offsetPosition.y + (ElementMover.#mouse.y - parseInt(translate[1]))
-	//	}
-	//	ElementMover.#mouse.x = pEvent.pageX + window.scrollX - ElementMover.#offsetPosition.x
-	//	ElementMover.#mouse.y = pEvent.pageY + window.scrollY - ElementMover.#offsetPosition.y
-	//	if (pEvent.pressure !== 0 && ElementMover.#isPointerDown) ElementMover.#element.style.translate = `${ElementMover.#mouse.x}px ${ElementMover.#mouse.y}px`
-	// }
-	//
-	// static async #pointerUp () {
-	//	await ElementMover.#callBack(ElementMover.#mouse)
-	//	ElementMover.#resetMousePosition()
-	//	document.body.removeEventListener('pointermove', ElementMover.#pointerMove)
-	//	ElementMover.#selector.removeEventListener('pointerup', ElementMover.#pointerUp)
-	//	ElementMover.#isPointerDown = false
-	// }
-	//
-	// static #keyDown (pEvent) {
-	//	ElementMover.#keysPress[pEvent.code] = true
-	// }
-	//
-	// static #keyUp (pEvent) {
-	//	const translate = ElementMover.#element.style.translate.split(' ')
-	//	let translateX = parseInt(translate[0])
-	//	let translateY = parseInt(translate[1])
-	//	const offset = ElementMover.#keysPress.ShiftLeft ? 10 : ElementMover.#keysPress.ControlLeft ? 50 : 1
-	//	if (pEvent.code === 'ArrowUp') translateY -= offset
-	//	if (pEvent.code === 'ArrowDown') translateY += offset
-	//	if (pEvent.code === 'ArrowRight') translateX += offset
-	//	if (pEvent.code === 'ArrowLeft') translateX -= offset
-	//	setTimeout(() => {
-	//		ElementMover.#element.style.translate = `${translateX}px ${translateY}px`
-	//	})
-	//	ElementMover.#keysPress[pEvent.code] = false
-	//	ElementMover.#callBack({ x: translateX, y: translateY })
-	// }
+	static async #pointerDown (pEvent) {
+		ElementResizer.#mouse.originalX = pEvent.pageX
+		ElementResizer.#mouse.originalY = pEvent.pageY
+		ElementResizer.#selector = pEvent.target
+		ElementResizer.leftHorizontalMove = pEvent.target.className.includes('left')
+		ElementResizer.rightHorizontalMove = pEvent.target.className.includes('right')
+		ElementResizer.topVerticalMove = pEvent.target.className.includes('Top')
+		ElementResizer.bottomVerticalMove = pEvent.target.className.includes('Bottom')
+		// pEvent.target.addEventListener('pointerup', ElementResizer.#pointerUp)
+		document.body.addEventListener('pointerup', ElementResizer.#pointerUp)
+		ElementResizer.#isPointerDown = true
+
+		ElementResizer.width = ElementResizer.#element.getBoundingClientRect().width
+		ElementResizer.height = ElementResizer.#element.getBoundingClientRect().height
+		// const translate = ElementResizer.#element.style.translate.split(' ')
+		// ElementResizer.#mouse.translateX = parseInt(translate[0])
+		// ElementResizer.#mouse.translateY = parseInt(translate[1])
+	}
+
+	static #pointerMove (pEvent) {
+		pEvent.returnValue = false
+		ElementResizer.#mouse.x = pEvent.pageX - ElementResizer.#mouse.originalX
+		ElementResizer.#mouse.y = pEvent.pageY - ElementResizer.#mouse.originalY
+		const translate = ElementResizer.#element.style.translate.split(' ')
+		// ElementResizer.#offsetPosition = {
+		//	x: ElementResizer.#offsetPosition.x + (ElementResizer.#mouse.x - parseInt(translate[0])),
+		//	y: ElementResizer.#offsetPosition.y + (ElementResizer.#mouse.y - parseInt(translate[1]))
+		// }
+		ElementResizer.#mouse.translateX = parseInt(translate[0])
+		ElementResizer.#mouse.translateY = parseInt(translate[1])
+		if (pEvent.pressure !== 0 && ElementResizer.#isPointerDown) {
+			if (ElementResizer.leftHorizontalMove) {
+				// TODO pb probable car mouvement après le clic dans certaines position + peut etre mutualisation possible avec move ?
+				ElementResizer.#mouse.translateX = pEvent.pageX + window.scrollX + 20
+				ElementResizer.#element.querySelector('input').style.width = ElementResizer.width - ElementResizer.#mouse.x + 'px'
+			}
+			if (ElementResizer.rightHorizontalMove) ElementResizer.#element.querySelector('input').style.width = ElementResizer.width + ElementResizer.#mouse.x + 'px'
+			if (ElementResizer.topVerticalMove) {
+				ElementResizer.#mouse.translateY = pEvent.pageY + window.scrollY + 20
+				ElementResizer.#element.querySelector('input').style.height = ElementResizer.height - ElementResizer.#mouse.y + 'px'
+			}
+			if (ElementResizer.bottomVerticalMove) ElementResizer.#element.querySelector('input').style.height = ElementResizer.height + ElementResizer.#mouse.y + 'px'
+			ElementResizer.#element.style.translate = `${ElementResizer.#mouse.translateX}px ${ElementResizer.#mouse.translateY}px`
+		}
+	}
+
+	static async #pointerUp (pEvent) {
+		await ElementResizer.#callBack({
+			x: ElementResizer.#mouse.translateX,
+			y: ElementResizer.#mouse.translateY,
+			width: parseInt(ElementResizer.#element.querySelector('input').style.width),
+			height: parseInt(ElementResizer.#element.querySelector('input').style.height)
+		})
+		// ElementResizer.#resetMousePosition()
+		console.log(ElementResizer.#selector)
+		document.body.removeEventListener('pointermove', ElementResizer.#pointerMove)
+		document.body.removeEventListener('pointerup', ElementResizer.#pointerUp)
+		ElementResizer.#selector.removeEventListener('pointerup', ElementResizer.#pointerUp)
+		ElementResizer.#isPointerDown = false
+	}
 }
