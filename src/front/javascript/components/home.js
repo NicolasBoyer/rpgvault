1011c623a6860e5aaa454f574382e270e3b2976d
Nicolas <elendil_le_grand@hotmail.com>
2023-01-26 18:33:55 +0100

    debug pour mon ipad

--- src/front/javascript/components/home.js
+++ src/front/javascript/components/home.js
@@ -24,13 +24,12 @@ export default class Home extends HTMLElement {
 
 	#initParchment () {
 		const main = document.querySelector('#main')
-		const bodyHeight = document.body.getBoundingClientRect().height
-		main.style.height = `${bodyHeight}px`
+		document.body.style.height = `${Math.max(document.body.getBoundingClientRect().height, window.innerHeight)}px`
 		const mainSize = main.getBoundingClientRect()
 		const parchment = document.querySelector('#parchment')
 		if (!parchment) return
 		parchment.style.width = `${mainSize.width}px`
-		parchment.style.height = `${Math.max(mainSize.height, bodyHeight - mainSize.top - parseInt(getComputedStyle(main).marginBottom))}px`
+		parchment.style.height = main.style.height = `${Math.max(mainSize.height, document.body.getBoundingClientRect().height - mainSize.top - parseInt(getComputedStyle(main).marginBottom))}px`
 		// parchment.style.top = `${mainSize.top}px`
 		// parchment.style.left = `${mainSize.left}px`
 		this.#render()
