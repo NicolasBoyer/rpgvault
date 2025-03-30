06d6222b9b541c31b818c4e2ee0892a4addb4560
Nicolas <elendil_le_grand@hotmail.com>
2025-03-31 00:28:33 +0200

    todo

--- src/front/javascript/components/sheets.ts
+++ src/front/javascript/components/sheets.ts
@@ -16,6 +16,10 @@ import { User } from '../classes/user.js'
 // TODO système de calque ou d'index
 // TODO Avatar
 // TODO voir si intéressant de créer un ensemble permettant d'aller plus vite dans la création de nouveaux éléments mais pas sur que ce soit faisable
+// TODO rester appuyer fait bouger l'élément
+// TODO pouvoir sélectionner plusieurs éléments
+// TODO ajouter un élément liste
+// Todo ajouter du survol avec points de règles ...
 export default class Sheets extends HTMLElement {
     private sheets: TSheet[] = []
     private editMode: string | null = null
